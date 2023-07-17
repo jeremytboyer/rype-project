0c8dc4a621b5adec1c31ca4f23a89d2fbdf44559
hsaagal78 <hsagal78@gmail.com>
2023-07-17 14:51:28 -0400

    try to push and se if works

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('user_app_example', 'root', '', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql'
 });
 
