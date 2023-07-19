fc744ebf7fe0efb8e7b137be3ce37ffdc6547711
Jeremy Boyer <jeremytboyer@gmail.com>
2023-07-19 17:31:16 -0400

    trying to deploy

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('rype_app_db', 'root', '', {
+console.log(process.env.JAWSDB_URL);
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('rype_app_db', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql'
 });
