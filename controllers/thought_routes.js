64cbb8fd331b4c129a730ba439966647f8db5fe3
hsaagal78 <hsagal78@gmail.com>
2023-07-19 13:21:50 -0400

    added removeCustomR

--- controllers/thought_routes.js
+++ controllers/thought_routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const User = require('../models/User');
 const Thought = require('../models/Thought');
+
 
 function isAuthenticated(req, res, next) {
   const isAuthenticated = req.session.user_id;
@@ -10,15 +10,43 @@ function isAuthenticated(req, res, next) {
   next();
 }
 
-
 // Add a thought
 router.post('/thought', isAuthenticated, async (req, res) => {
   await Thought.create({
+    title: req.body.title,
     text: req.body.text,
-    userId: req.session.user_id
+    userId: req.session.user_id,
   });
 
   res.redirect('/dashboard');
+
 });
+
+router.delete('/dashboard/:id', async (req, res) => {
+  try {
+  //   const recipeId = req.params.id;
+  //   console.log('Recipe ID received:', recipeId);
+
+    const recipe = await Favorite.destroy({
+      where: {
+          recipeId: req.params.id,
+          // user_id: req.session.user_id,
+      },
+  });
+    console.log('Recipe ID received:', recipe);
+    if (!recipe) {
+     
+      return res.status(404).json({ message: 'Recipe not found' });
+    }
+
+  //   await recipe.destroy();
+
+    res.status(204).end();
+  } catch (error) {
+    console.error('Error deleting favorite recipe:', error);
+    res.status(500).json({ message: 'Error deleting favorite recipe' });
+  }
+});
+
 
 module.exports = router;
--- models/Thought.js
+++ models/Thought.js
@@ -1,19 +1,38 @@
 const { Model, DataTypes } = require('sequelize');
 const db = require('../db/connection');
 
+
 class Thought extends Model { }
 
+
 Thought.init({
-  text: {
+  title: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      min: 3
-    }
-  }
-}, {
+      notEmpty: true,
+    },
+  },
+  text: {
+    type: DataTypes.TEXT('medium'),
+    allowNull: false,
+    validate: {
+      min: 3,
+    },
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW, 
+  },
+
+
+}, 
+{
   sequelize: db,
-  modelName: 'thought'
+  modelName: 'thought',
+ 
 });
+
 
 module.exports = Thought;
