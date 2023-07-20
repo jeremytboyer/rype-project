0f0c98e32b7d6852911c65e59d9e9b6959299990
Jeremy Boyer <jeremytboyer@gmail.com>
2023-07-20 11:23:04 -0400

    ready for deploy

--- models/User.js
+++ models/User.js
@@ -1,56 +1,52 @@
-const { Model, DataTypes } = require('sequelize');
-const { hash, compare } = require('bcrypt');
-const db = require('../db/connection');
-const Thought = require('./Thought');
-const Favorite = require('./Favorite')
+const { Model, DataTypes } = require("sequelize");
+const { hash, compare } = require("bcrypt");
+const db = require("../db/connection");
+const Thought = require("./Thought");
+const Favorite = require("./Favorite");
 
-class User extends Model { }
+class User extends Model {}
 
-User.init({
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        min: 6,
+      },
+    },
+    profileImageUrl: {
+      type: DataTypes.STRING, // This field will store the Cloudinary URL
+    },
   },
-  password: {
-    type: DataTypes.STRING,
-    validate: {
-      min: 6
-    }
-  },
-  profileImageUrl: {
-    type: DataTypes.STRING, // This field will store the Cloudinary URL
-  },
-  
-}, {
-  sequelize: db,
-  modelName: 'user',
-  hooks: {
-    async beforeCreate(user) {
-      const hashPassword = await hash(user.password, 10);
-      user.password = hashPassword;
-    }
-  },
-
-
-});
+  {
+    sequelize: db,
+    modelName: "user",
+    hooks: {
+      async beforeCreate(user) {
+        const hashPassword = await hash(user.password, 10);
+        user.password = hashPassword;
+      },
+    },
+  }
+);
 
 User.prototype.validatePass = async function (formPassword) {
   const isValid = await compare(formPassword, this.password);
-
   return isValid;
-}
+};
 
-
-
-User.hasMany(Favorite)
-Favorite.belongsTo(User)
+User.hasMany(Favorite);
+Favorite.belongsTo(User);
 
 User.hasMany(Thought);
 Thought.belongsTo(User);
-
 
 module.exports = User;
